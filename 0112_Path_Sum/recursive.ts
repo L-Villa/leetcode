307e6fbe094204238e8adadd8e59c1a4bac4996c
L-Villa <63620359+L-Villa@users.noreply.github.com>
2021-12-06 22:14:52 -0800

    Added 112

---
+++ 0112_Path_Sum/recursive.ts
@@ -0,0 +1,8 @@
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+  if (!root) return false;
+  if (root.val === targetSum && !root.left && !root.right) return true;
+  return (
+    hasPathSum(root.left, targetSum - root.val) ||
+    hasPathSum(root.right, targetSum - root.val)
+  );
+}
