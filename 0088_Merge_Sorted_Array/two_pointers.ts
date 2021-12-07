797466fb52052475f01f0546a21aca7ff43baf7c
L-Villa <63620359+L-Villa@users.noreply.github.com>
2021-12-06 21:37:20 -0800

    Added 88

---
+++ 0088_Merge_Sorted_Array/two_pointers.ts
@@ -0,0 +1,18 @@
+/**
+  Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+  m--;
+  n--;
+  let i = nums1.length - 1;
+  while (i >= 0) {
+    if (n > -1 && m > -1 && nums1[m] >= nums2[n]) {
+      nums1[i] = nums1[m];
+      nums1[m] = nums2[n];
+      m--;
+    } else if (n > -1) {
+      nums1[i] = nums2[n--];
+    }
+    i--;
+  }
+}
